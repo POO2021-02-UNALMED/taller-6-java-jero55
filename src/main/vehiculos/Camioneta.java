ae76b986ed5735922c9e36770188419954b6405f
jero55 <jsalazars@unal.edu.co>
2021-11-28 11:14:33 -0500

    Create Camioneta.java

---
+++ src/main/vehiculos/Camioneta.java
@@ -0,0 +1,26 @@
+package vehiculos;
+
+public class Camioneta extends Vehiculo {
+
+	protected boolean volco;
+	private static int cantidadCamionetas;
+
+	public Camioneta(String placa, int puertas, int velocidadMaxima, String nombre, int precio, int peso,String traccion, Fabricante fabricante, boolean volco) {
+		super( placa, puertas, 90, nombre,precio, peso,"4X4", fabricante);
+		this.volco = volco;
+	}
+
+	public boolean isVolco() {
+		return volco;
+	}
+
+	public void setVolco(boolean volco) {
+		this.volco = volco;
+	}
+	
+	public static int getCantidadCamionetas() {
+		return Camioneta.cantidadCamionetas;
+	}
+	
+	
+}
